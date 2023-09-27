060f903084763da2cda7abf223df38c9cef2fa8e
ashleyshakir <ashleymshakir@gmail.com>
2023-09-27 16:55:56 -0400

    constructor, getters and setters, and toString methods added to photo model

--- photo-fiesta/src/main/java/com/example/photofiesta/models/Photo.java
+++ photo-fiesta/src/main/java/com/example/photofiesta/models/Photo.java
@@ -18,4 +18,56 @@ public class Photo {
 
     @Column
     private String imageUrl;
+
+    public Photo() {
+    }
+
+    public Photo(Long id, String name, String description, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Photo{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
 }
