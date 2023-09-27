b0be6dcb63e338264c6e7d10a42416605c40afc1
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-27 16:50:37 -0400

    Add constructors, getters and setters to Album model

--- photo-fiesta/src/main/java/com/example/photofiesta/models/Album.java
+++ photo-fiesta/src/main/java/com/example/photofiesta/models/Album.java
@@ -16,6 +16,45 @@ public class Album {
     @Column
     private String description;
 
+    public Album() {
+    }
 
+    public Album(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Album{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
