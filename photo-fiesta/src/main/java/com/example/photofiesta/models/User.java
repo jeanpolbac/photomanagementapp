24e8abb83b69e710915eb8fe76f17b33b1c89a1c
Ricardo Maya <rickmaya@gmail.com>
2023-09-27 15:21:21 -0500

    add id property to User model

--- photo-fiesta/src/main/java/com/example/photofiesta/models/User.java
+++ photo-fiesta/src/main/java/com/example/photofiesta/models/User.java
@@ -1,4 +1,12 @@
 package com.example.photofiesta.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
