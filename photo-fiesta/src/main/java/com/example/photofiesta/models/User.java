d8de56c5a6c2298f99c3b74462843ac0063f81b4
Ricardo Maya <rickmaya@gmail.com>
2023-09-27 15:24:26 -0500

    add username, emailAddress, and password properties to user model

--- photo-fiesta/src/main/java/com/example/photofiesta/models/User.java
+++ photo-fiesta/src/main/java/com/example/photofiesta/models/User.java
@@ -1,5 +1,7 @@
 package com.example.photofiesta.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.persistence.*;
 
 @Entity
@@ -9,4 +11,14 @@ public class User {
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
+    private String userName;
+
+    @Column(unique = true)
+    private String emailAddress;
+
+    @Column
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
 }
