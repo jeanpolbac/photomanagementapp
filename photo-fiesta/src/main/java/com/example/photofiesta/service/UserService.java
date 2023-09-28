640bfb2eeb615987e7d51cd3540afbdb40c258a0
Ricardo Maya <rickmaya@gmail.com>
2023-09-28 11:16:38 -0500

    refactor 'createUser' method in UserService

--- photo-fiesta/src/main/java/com/example/photofiesta/service/UserService.java
+++ photo-fiesta/src/main/java/com/example/photofiesta/service/UserService.java
@@ -2,7 +2,11 @@ package com.example.photofiesta.service;
 
 import com.example.photofiesta.models.User;
 import com.example.photofiesta.repository.UserRepository;
+import com.example.photofiesta.security.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,13 +14,23 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
+    private final JWTUtils jwtUtils;
+
+    private final AuthenticationManager authenticationManager;
+
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder, JWTUtils jwtUtils, @Lazy AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
     }
 
     public User createUser(User userObject) {
         if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
+            userObject.setPassword(passwordEncoder.encode((userObject.getPassword())));
             return userRepository.save(userObject);
         } else {
             throw new RuntimeException("User already exists"); //ToDo update with InformationExistException
