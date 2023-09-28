057528b43c1d49ff634fa7100e2b09057c17e0fb
Ricardo Maya <rickmaya@gmail.com>
2023-09-28 11:26:34 -0500

    create 'LoginRequest' model

---
+++ photo-fiesta/src/main/java/com/example/photofiesta/models/request/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.example.photofiesta.models.request;
+
+public class LoginRequest {
+    private String emailAddress;
+
+    private String password;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
