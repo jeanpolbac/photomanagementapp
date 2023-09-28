934331f493c94450a6c067db3aa5cd2e7ced4249
ashleyshakir <ashleymshakir@gmail.com>
2023-09-28 12:52:02 -0400

    Java docs added to SecurityConfiguration class

--- photo-fiesta/src/main/java/com/example/photofiesta/security/SecurityConfiguration.java
+++ photo-fiesta/src/main/java/com/example/photofiesta/security/SecurityConfiguration.java
@@ -15,32 +15,36 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
 
+    // Bean for creating a BCryptPasswordEncoder to hash passwords.
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-
+    // Bean for creating the JwtRequestFilter used for JWT authentication.
     @Bean
     public JwtRequestFilter authJwtRequestFilter() {
         return new JwtRequestFilter();
     }
 
+    // Bean for configuring the security filter chain.
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Define which endpoints are accessible without authentication.
         http.authorizeRequests().antMatchers("/auth/users", "/auth/users/login/", "/auth/users/register/").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
                 .antMatchers("/helloworld/").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Set session creation policy to STATELESS.
                 .and().csrf().disable()
-                .headers().frameOptions().disable();
+                .headers().frameOptions().disable(); // Disable frame options for h2-console.
+        // Add the JwtRequestFilter before the default UsernamePasswordAuthenticationFilter.
         http.addFilterBefore(authJwtRequestFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
-
+    // Bean for creating an AuthenticationManager.
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
