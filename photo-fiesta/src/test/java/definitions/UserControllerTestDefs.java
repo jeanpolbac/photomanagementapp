f471b85a3133d2049aec226ce79ad2ba37e18a32
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-28 16:15:23 -0400

    Create UserControllerTestDefs

---
+++ photo-fiesta/src/test/java/definitions/UserControllerTestDefs.java
@@ -0,0 +1,50 @@
+package definitions;
+
+import com.example.photofiesta.PhotoFiestaApplication;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Logger;
+
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = PhotoFiestaApplication.class)
+
+
+public class UserControllerTestDefs {
+
+    private static final Logger logger = Logger.getLogger(UserControllerTestDefs.class.getName());
+
+    private static final String BASE_URL = "http://localhost:";
+    private static final String basePath = "/auth/hello/";
+
+    @LocalServerPort
+    String port;
+
+    private static Response response;
+
+    @Given("A valid public endpoint")
+    public void aValidPublicEndpoint() {
+        ResponseEntity<String> responseEntity = new RestTemplate().exchange(BASE_URL + port + basePath, HttpMethod.GET, null, String.class);
+        Assert.assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
+    }
+
+    @When("I say hello")
+    public void iSayHello() {
+
+    }
+
+    @Then("Hello is shown")
+    public void helloIsShown() {
+    }
+}
+
