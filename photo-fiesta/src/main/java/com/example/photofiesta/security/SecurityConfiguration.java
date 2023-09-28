4f1f9dce8d024965f93e74fd978831d871ed350f
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-28 12:11:40 -0400

    Add AuthenticationManager to SecurityConfiguration

--- photo-fiesta/src/main/java/com/example/photofiesta/security/SecurityConfiguration.java
+++ photo-fiesta/src/main/java/com/example/photofiesta/security/SecurityConfiguration.java
@@ -2,6 +2,8 @@ package com.example.photofiesta.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -39,4 +41,8 @@ public class SecurityConfiguration {
     }
 
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
 }
