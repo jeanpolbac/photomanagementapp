343909c0a8369d1eda2cbc339bd2ca25021f8108
ashleyshakir <ashleymshakir@gmail.com>
2023-09-28 12:45:32 -0400

    Java docs added to JWTUtils class

--- photo-fiesta/src/main/java/com/example/photofiesta/security/JWTUtils.java
+++ photo-fiesta/src/main/java/com/example/photofiesta/security/JWTUtils.java
@@ -8,6 +8,9 @@ import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Utility class for handling JSON Web Tokens (JWT) in the application.
+ */
 @Service
 public class JWTUtils {
     Logger logger = Logger.getLogger(JWTUtils.class.getName());
@@ -18,6 +21,12 @@ public class JWTUtils {
     @Value("${jwt-expiration-ms}")
     private int jwtExpMS;
 
+    /**
+     * Generate a JWT token based on user details.
+     *
+     * @param myUserDetails The user details for whom the token is generated.
+     * @return A JWT token as a String.
+     */
     public String generateJwtToken(MyUserDetails myUserDetails) {
         return Jwts.builder()
                 .setSubject(myUserDetails.getUsername())
@@ -26,7 +35,12 @@ public class JWTUtils {
                 .signWith(SignatureAlgorithm.HS256, jwtSecret)
                 .compact();
     }
-
+    /**
+     * Extract the username from a JWT token.
+     *
+     * @param token The JWT token from which to extract the username.
+     * @return The username as a String.
+     */
     public String getUserNameFromJwtToken(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(jwtSecret)
@@ -35,7 +49,12 @@ public class JWTUtils {
                 .getBody()
                 .getSubject();
     }
-
+    /**
+     * Validate the authenticity and expiration of a JWT token.
+     *
+     * @param token The JWT token to validate.
+     * @return true if the token is valid, false otherwise.
+     */
     public boolean validateJwtToken(String token) {
         try {
             Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
