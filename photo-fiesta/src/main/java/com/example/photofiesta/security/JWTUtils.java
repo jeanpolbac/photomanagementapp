1b3e515784ad145ab452e1181eafd6cf4060e6a7
Ricardo Maya <rickmaya@gmail.com>
2023-09-28 10:36:56 -0500

    create jwt utils class with generateJwtToken method

---
+++ photo-fiesta/src/main/java/com/example/photofiesta/security/JWTUtils.java
@@ -0,0 +1,29 @@
+package com.example.photofiesta.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.logging.Logger;
+
+@Service
+public class JWTUtils {
+    Logger logger = Logger.getLogger(JWTUtils.class.getName());
+
+    @Value("${jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${jwt-expiration-ms}")
+    private int jwtExpMS;
+
+    public String generateJwtToken(MyUserDetails myUserDetails) {
+        return Jwts.builder()
+                .setSubject(myUserDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpMS))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
+}
