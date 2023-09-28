5a2cfe122b8a3456840737bbf15149dd733f8849
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-28 14:28:42 -0400

    Create User Controller with getHello method for testing

---
+++ photo-fiesta/src/main/java/com/example/photofiesta/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.photofiesta.controller;
+
+import com.example.photofiesta.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/auth/users")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(path = "/auth/hello/")
+    public String getHello() {
+        return "Hello";
+    }
+}
