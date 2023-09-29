cc1637c8aa733c868a27256d1a2fd3b80569fe9a
Ricardo Maya <rickmaya@gmail.com>
2023-09-29 16:39:44 -0500

    refactor getUserAlbums by getting currently logged in user

--- photo-fiesta/src/main/java/com/example/photofiesta/service/AlbumService.java
+++ photo-fiesta/src/main/java/com/example/photofiesta/service/AlbumService.java
@@ -1,8 +1,12 @@
 package com.example.photofiesta.service;
 
+import com.example.photofiesta.exception.InformationNotFoundException;
 import com.example.photofiesta.models.Album;
+import com.example.photofiesta.models.User;
 import com.example.photofiesta.repository.AlbumRepository;
+import com.example.photofiesta.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,7 +21,16 @@ public class AlbumService {
         this.albumRepository = albumRepository;
     }
 
-    public List<Album> getUserAlbums(Long userId) {
-        return albumRepository.findByUserId(userId);
+    public List<Album> getUserAlbums() {
+        List<Album> albumList = albumRepository.findByUserId(AlbumService.getCurrentLoggedInUser().getId());
+        if (albumList.isEmpty()) {
+            throw new InformationNotFoundException("no albums found for user id " + AlbumService.getCurrentLoggedInUser().getId());
+        }
+        return albumList;
+    }
+
+    public static User getCurrentLoggedInUser() {
+        MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userDetails.getUser();
     }
 }
