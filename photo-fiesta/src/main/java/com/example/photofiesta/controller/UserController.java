87150fbf50d06c65f5cb8e8392924acf8db45974
ashleyshakir <ashleymshakir@gmail.com>
2023-09-29 10:49:20 -0400

    loginUser endpoint method created in UserController

--- photo-fiesta/src/main/java/com/example/photofiesta/controller/UserController.java
+++ photo-fiesta/src/main/java/com/example/photofiesta/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.photofiesta.controller;
 
 import com.example.photofiesta.models.User;
+import com.example.photofiesta.models.request.LoginRequest;
+import com.example.photofiesta.models.response.LoginResponse;
 import com.example.photofiesta.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,13 +10,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.Optional;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping(path = "/auth/users")
 public class UserController {
 
     private UserService userService;
-
+    Logger logger = Logger.getLogger(UserController.class.getName());
     static HashMap<String, Object> message = new HashMap<>();
 
     @Autowired
@@ -39,6 +43,19 @@ public class UserController {
         } else {
             message.put("message", "email already exists");
             return new ResponseEntity<>(message, HttpStatus.OK);
+        }
+    }
+
+    @PostMapping(path = "/login/")
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest){
+        Optional<String> jwtToken = userService.loginUser(loginRequest);
+        if(jwtToken.isPresent()){
+            logger.info("Authentication is good for user " + loginRequest.getEmailAddress());
+            return ResponseEntity.ok(new LoginResponse(jwtToken.get()));
+        }
+        else{
+            logger.warning("Authentication failed for user " + loginRequest.getEmailAddress());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new LoginResponse("Authentication failed"));
         }
     }
 }
