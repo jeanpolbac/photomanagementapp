e1eecbe3f2ca9723c67d844dc24726c0e805b700
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-09-30 17:08:31 -0400

    Complete getUserAlbum endpoint

--- photo-fiesta/src/main/java/com/example/photofiesta/controller/AlbumController.java
+++ photo-fiesta/src/main/java/com/example/photofiesta/controller/AlbumController.java
@@ -1,5 +1,6 @@
 package com.example.photofiesta.controller;
 
+import com.example.photofiesta.models.Album;
 import com.example.photofiesta.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,18 +9,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/api")
 public class AlbumController {
 
+    @Autowired
     private AlbumService albumService;
+    static HashMap<String, Object> message = new HashMap<>();
 
     @GetMapping("/albums/")
     public ResponseEntity<?> getUserAlbums() {
-
-        return ResponseEntity.ok("You have photo albums available");
+        List<Album> albumList = albumService.getUserAlbums();
+        if (albumList.isEmpty()) {
+            message.put("message", "Cannot find any albums");
+            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        } else {
+            message.put("message", "Able to retrieve albums");
+            message.put("data", albumList);
+            return new  ResponseEntity<>(message, HttpStatus.OK);
+        }
     }
-
-
 
 }
