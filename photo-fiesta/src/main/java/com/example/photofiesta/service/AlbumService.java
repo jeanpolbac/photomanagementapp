c699219918af17fc322a89e59d2123e302f2bbee
ashleyshakir <ashleymshakir@gmail.com>
2023-10-01 10:59:16 -0400

    Add createAlbumPhoto business logic to AlbumService

--- photo-fiesta/src/main/java/com/example/photofiesta/service/AlbumService.java
+++ photo-fiesta/src/main/java/com/example/photofiesta/service/AlbumService.java
@@ -1,9 +1,12 @@
 package com.example.photofiesta.service;
 
+import com.example.photofiesta.exception.InformationExistException;
 import com.example.photofiesta.exception.InformationNotFoundException;
 import com.example.photofiesta.models.Album;
+import com.example.photofiesta.models.Photo;
 import com.example.photofiesta.models.User;
 import com.example.photofiesta.repository.AlbumRepository;
+import com.example.photofiesta.repository.PhotoRepository;
 import com.example.photofiesta.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,10 +18,12 @@ import java.util.List;
 public class AlbumService {
 
     private final AlbumRepository albumRepository;
+    private final PhotoRepository photoRepository;
 
     @Autowired
-    public AlbumService(AlbumRepository albumRepository) {
+    public AlbumService(AlbumRepository albumRepository, PhotoRepository photoRepository) {
         this.albumRepository = albumRepository;
+        this.photoRepository = photoRepository;
     }
 
     public List<Album> getUserAlbums() {
@@ -32,5 +37,15 @@ public class AlbumService {
     public static User getCurrentLoggedInUser() {
         MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return userDetails.getUser();
+    }
+
+    public Photo createAlbumPhoto(Photo photoObject) {
+        Album album = getCurrentLoggedInUser().getAlbumList().get(0);
+        Photo photo = photoRepository.findByImageUrlAndAlbumId(photoObject.getImageUrl(),album.getId());
+        if(photo != null){
+            throw new InformationExistException("A photo with the image url " + photoObject.getImageUrl() + " already exists!");
+        }
+        photoObject.setAlbum(album);
+        return photoRepository.save(photoObject);
     }
 }
