c0465156b030527a8f77f2520a982f4837f0dd19
Ricardo Maya <rickmaya@gmail.com>
2023-10-01 10:50:30 -0500

    create basic 'createAlbum' controller to test the endpoint

--- photo-fiesta/src/main/java/com/example/photofiesta/controller/AlbumController.java
+++ photo-fiesta/src/main/java/com/example/photofiesta/controller/AlbumController.java
@@ -5,9 +5,7 @@ import com.example.photofiesta.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -31,6 +29,11 @@ public class AlbumController {
             message.put("data", albumList);
             return new  ResponseEntity<>(message, HttpStatus.OK);
         }
+    }
+
+    @PostMapping("/albums/")
+    public ResponseEntity<?> createAlbum() {
+        return ResponseEntity.ok("you've reached create album");
     }
 
 }
