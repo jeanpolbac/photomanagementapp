64bf314ecc31a54eab7dd55b2e021c3eaf0456e0
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-01 19:59:08 -0400

    Add logging to improve test and code debugging

--- photo-fiesta/src/test/java/definitions/AlbumManagementTestDefs.java
+++ photo-fiesta/src/test/java/definitions/AlbumManagementTestDefs.java
@@ -1,6 +1,5 @@
 package definitions;
 
-import com.example.photofiesta.models.Album;
 import com.example.photofiesta.repository.AlbumRepository;
 import com.example.photofiesta.service.AlbumService;
 import io.cucumber.java.en.And;
@@ -35,7 +34,7 @@ public class AlbumManagementTestDefs extends TestSetupDefs {
 
 
     public String getJWTKey() throws JSONException {
-
+        logger.info("TestJWTKey: Generated");
         // Set the base URI and create a request
         RestAssured.baseURI = BASE_URL;
         RequestSpecification request = RestAssured.given();
@@ -58,27 +57,39 @@ public class AlbumManagementTestDefs extends TestSetupDefs {
 
     @Given("A logged in user")
     public void aLoggedInUser() throws JSONException {
+        logger.info("Step: A logged in user");
         RequestSpecification request = RestAssured.given();
         request.header("Authorization", "Bearer " + getJWTKey());
     }
 
     @And("A list of albums are available")
     public void aListOfAlbumsAreAvailable() throws JSONException {
+        logger.info("Step: A list of albums are available");
+        // Obtain the JWT token for authorization
         String jwtToken = getJWTKey();
+
+        // Create HTTP headers with the JWT token for authentication
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer " + jwtToken);
+
+        // Create an HTTP entity with the headers
         HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+
         try {
+            // Send a GET request to retrieve the list of albums
             ResponseEntity<String> response = new RestTemplate().exchange(BASE_URL + port + "/api/albums/", HttpMethod.GET, entity, String.class);
             String responseBody = String.valueOf(response.getBody());
             logger.info("Response body: " + responseBody);
-            if (responseBody != null && !responseBody.isEmpty()) {
 
+            if (responseBody != null && !responseBody.isEmpty()) {
+                // Parse the JSON response to extract the list of albums
                 List<Map<String, String>> albums = JsonPath.from(String.valueOf(response.getBody())).get("data");
 
+                // Assert that the response status is OK and there are albums available
                 Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
                 Assert.assertTrue(albums.size() > 0);
             } else {
+                // If the response is null or empty, fail the test
                 Assert.fail("Received null or empty response");
             }
         } catch (HttpClientErrorException e) {
@@ -89,12 +100,13 @@ public class AlbumManagementTestDefs extends TestSetupDefs {
 
     @When("I add a album to my list")
     public void iAddAAlbumToMyList() throws JSONException {
+        logger.info("Step: I add a album to my list");
         // Creating authorization and content type
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Bearer " + getJWTKey());
         headers.add("Content-Type", "application/json");
 
-        // Creating object to pass through
+        // Creating an object to pass through
         JSONObject requestBody = new JSONObject();
         requestBody.put("name", "New Test Album");
         requestBody.put("description", "New Description");
@@ -107,26 +119,32 @@ public class AlbumManagementTestDefs extends TestSetupDefs {
 
     @Then("The album is added")
     public void theAlbumIsAdded() {
+        logger.info("Step: The album is added");
         Assert.assertEquals(HttpStatus.CREATED, response.getStatusCode());
     }
 
 
     @When("I delete an album in my list")
     public void iDeleteAnAlbumInMyList() throws JSONException {
+        logger.info("Step: I delete an album in my list");
         // Creating authorization and content type
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + getJWTKey());
-        headers.add("Content-Type", "application/json");
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", "Bearer " + getJWTKey());
+            headers.add("Content-Type", "application/json");
 
-        // Build our post request
-        HttpEntity<String> entity = new HttpEntity<>(null, headers);
-        response = new RestTemplate().exchange(BASE_URL + port + "/api/albums/1", HttpMethod.DELETE, entity, String.class);
-
-
+            // Build our post request
+            HttpEntity<String> entity = new HttpEntity<>(null, headers);
+            response = new RestTemplate().exchange(BASE_URL + port + "/api/albums/1", HttpMethod.DELETE, entity, String.class);
+            logger.info("Response Status: " + response.getStatusCode());
+        } catch (Exception e) {
+            logger.warning("Test: Error while deleting an album " + e.getMessage());
+        }
     }
 
 
     @Then("The album is removed")
     public void theAlbumIsRemoved() {
+        logger.info("Step: The album is removed");
         Assert.assertEquals(HttpStatus.OK, response.getStatusCode());    }
 }
