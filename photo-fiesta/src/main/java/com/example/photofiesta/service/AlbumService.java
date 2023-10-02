b815a7ad0e1bc953c8c958785ecb67c3319d45bd
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-01 21:32:20 -0400

    Add documentation to 'AlbumService' methods

--- photo-fiesta/src/main/java/com/example/photofiesta/service/AlbumService.java
+++ photo-fiesta/src/main/java/com/example/photofiesta/service/AlbumService.java
@@ -22,29 +22,52 @@ public class AlbumService {
         this.albumRepository = albumRepository;
     }
 
+    /**
+     * Get albums belonging to the current logged-in user
+     *
+     * @return List of user's albums
+     * @throws InformationNotFoundException if no albums are found
+     */
     public List<Album> getUserAlbums() {
         List<Album> albumList = albumRepository.findByUserId(AlbumService.getCurrentLoggedInUser().getId());
         if (albumList.isEmpty()) {
-            throw new InformationNotFoundException("no albums found for user id " + AlbumService.getCurrentLoggedInUser().getId());
+            throw new InformationNotFoundException("No albums found for user ID " + AlbumService.getCurrentLoggedInUser().getId());
         }
         return albumList;
     }
 
+    /**
+     * Create a new album
+     *
+     * @param album The album to create
+     * @return The created album
+     */
     public Album createAlbum(Album album) {
         return albumRepository.save(album);
     }
 
+    /**
+     * Delete an album by ID
+     *
+     * @param albumId The ID of the album to delete
+     * @return A message confirming the deletion
+     * @throws InformationNotFoundException if the album is not found
+     */
     public String deleteAlbum (Long albumId) {
         Optional<Album> album = albumRepository.findById(albumId);
         if (album.isPresent()) {
             albumRepository.deleteById(albumId);
             return "Album deleted" ;
         } else {
-            throw new InformationNotFoundException("Album id " + albumId + " not found!");
+            throw new InformationNotFoundException("Album ID " + albumId + " not found!");
         }
     }
 
-
+    /**
+     * Get the currently logged-in user
+     *
+     * @return The current user
+     */
     public static User getCurrentLoggedInUser() {
         MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return userDetails.getUser();
