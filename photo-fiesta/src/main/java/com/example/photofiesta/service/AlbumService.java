31ecc4fc985cfbac4278dac7f6d898265469f719
Ricardo Maya <rickmaya@gmail.com>
2023-10-02 09:34:39 -0500

    create service method to get all album photos

--- photo-fiesta/src/main/java/com/example/photofiesta/service/AlbumService.java
+++ photo-fiesta/src/main/java/com/example/photofiesta/service/AlbumService.java
@@ -1,25 +1,29 @@
 package com.example.photofiesta.service;
 
-import com.example.photofiesta.exception.InformationExistException;
 import com.example.photofiesta.exception.InformationNotFoundException;
 import com.example.photofiesta.models.Album;
+import com.example.photofiesta.models.Photo;
 import com.example.photofiesta.models.User;
 import com.example.photofiesta.repository.AlbumRepository;
+import com.example.photofiesta.repository.PhotoRepository;
 import com.example.photofiesta.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AlbumService {
 
     private final AlbumRepository albumRepository;
+    private final PhotoRepository photoRepository;
 
     @Autowired
-    public AlbumService(AlbumRepository albumRepository) {
+    public AlbumService(AlbumRepository albumRepository, PhotoRepository photoRepository) {
         this.albumRepository = albumRepository;
+        this.photoRepository = photoRepository;
     }
 
     public List<Album> getUserAlbums() {
@@ -37,5 +41,14 @@ public class AlbumService {
     public static User getCurrentLoggedInUser() {
         MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return userDetails.getUser();
+    }
+
+    public List<Photo> getAlbumPhotos(Long albumId) {
+        Optional<Album> optionalAlbum = Optional.ofNullable(albumRepository.findByIdAndUserId(albumId, 1L));
+        if (optionalAlbum.isPresent()) {
+            return optionalAlbum.get().getPhotoList();
+        } else {
+            throw new InformationNotFoundException("album with id: " + albumId + " doesn't exist");
+        }
     }
 }
